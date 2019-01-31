facbf1e4c49a62c0cc6c66d6c2d18aae5cdbbb00
Cornelis van der Bent <cornelis@meaning-matters.com>
2019-01-31 16:54:25 +0100

    Initial Commit

---
+++ uPortDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  uPortDemo
+//
+//  Created by Cornelis van der Bent on 31/01/2019.
+//  Copyright © 2019 ConsenSys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
